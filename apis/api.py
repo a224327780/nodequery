a7fdd11d2ad5cbab4c44b3db9064b1f1ec8d20a8
caoyufei <atcaoyufei@gmail.com>
2023-02-07 20:45:00 +0800

    0.13

--- apis/api.py
+++ apis/api.py
@@ -14,14 +14,17 @@ bp_api = Blueprint('api', url_prefix='api')
 @bp_api.websocket('/agent.list', name='agent_list')
 async def agent_list(request: Request, ws):
     col = DB.get_col()
-    while True:
-        data = []
-        async for item in col.find():
-            item = format_item(item)
-            data.append(item)
-        data = json.dumps(data)
-        await ws.send(data)
-        await asyncio.sleep(2)
+    try:
+        while True:
+            data = []
+            async for item in col.find():
+                item = format_item(item)
+                data.append(item)
+            data = json.dumps(data)
+            await ws.send(data)
+            await asyncio.sleep(2)
+    finally:
+        await ws.close()
 
 
 @bp_api.post('/agent.add')
@@ -39,22 +42,25 @@ async def agent(request: Request):
 
 @bp_api.websocket('/agent/<aid:str>', name='agent_data')
 async def agent(request: Request, ws, aid):
-    while True:
-        message = await ws.recv()
-        if not message:
-            break
+    try:
+        while True:
+            message = await ws.recv(10.0)
+            if not message:
+                break
 
-        if 'close' in message:
-            await ws.close()
-            break
+            if 'close' in message:
+                await ws.close()
+                break
 
-        if 'uptime=' in message:
-            col = DB.get_col()
-            data = dict(parse.parse_qsl(message))
-            data['update_date'] = get_bj_date()
-            await col.update_one({'_id': aid}, {'$set': data})
+            if 'uptime=' in message:
+                col = DB.get_col()
+                data = dict(parse.parse_qsl(message))
+                data['update_date'] = get_bj_date()
+                await col.update_one({'_id': aid}, {'$set': data})
 
-        await ws.send(message)
+            await ws.send(message)
+    finally:
+        await ws.close()
 
 
 def format_item(item):
@@ -63,7 +69,8 @@ def format_item(item):
     item['load'] = int(item['load_cpu'])
     # item['load'] = math.ceil(progress(item['load'].split(' ')[0], item['cpu_cores']))
     item['is_online'] = is_online(item['update_date'])
-    item['date'] = format_date(item['update_date'])
+    item['date'] = item['update_date']
+    item['online_day'] = int(int(item['uptime']) / (3600 * 24))
     item['rx_gap'] = format_size(int(item['rx_gap']))
     item['tx_gap'] = format_size(int(item['tx_gap']))
     return item
