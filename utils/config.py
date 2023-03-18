e5d7bf5d02a4a7368f59768cc4d74393c0b57608
caoyufei <atcaoyufei@gmail.com>
2023-03-18 22:55:24 +0800

    0.15

--- utils/config.py
+++ utils/config.py
@@ -6,7 +6,8 @@ dotenv.load_dotenv()
 FORWARDED_SECRET = '23bd9d81dc7c6e7b6ad103f1343979ab'
 REAL_IP_HEADER = 'X-Real-IP'
 PROXIES_COUNT = 1
-
+WEBSOCKET_MAX_SIZE = 2 ** 40
+WEBSOCKET_PING_INTERVAL = 30
 MONGO_URI = os.getenv('MONGO_URI', '')
 REDIS_URI = os.getenv('REDIS_URI', '')
 ENV = os.getenv('ENV', 'PROD')
